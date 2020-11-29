1bc7364e1bce0e6f313eaa07b4436ec54f403456
Denis <dllamoca665@gmail.com>
2020-11-29 17:06:33 -0500

    subir Icons

--- Piece.cpp
+++ Piece.cpp
@@ -3,7 +3,7 @@
 const QString Piece::BLACK_TEAM = "black_team";
 const QString Piece::WHITE_TEAM = "white_team";
 
-Piece::Piece(QWidget *parent) : QLabel(parent)
+Piece::Piece(QWidget *parent, QString teamValue) : QLabel(parent), team{teamValue}
 {
 
 }
